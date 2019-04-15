7b58b17512b74fd93300fe1d2e25df63de60c334
Maarten Breddels <maartenbreddels@gmail.com>
2019-04-15 15:04:28 +0200

    new: binby and better groupby

--- tests/datetime_test.py
+++ tests/datetime_test.py
@@ -1,14 +1,14 @@
 from common import *
 import numpy as np
-
+import pytest
 
 def test_datetime_operations():
     date = [np.datetime64('2009-10-12T03:31:00'),
-            np.datetime64('2016-02-11T10:17:34'),
-            np.datetime64('2015-11-12T11:34:22'),
-            np.datetime64('2003-03-03T00:33:15'),
-            np.datetime64('2014-07-23T15:08:05'),
-            np.datetime64('2011-01-01T07:02:01')]
+        np.datetime64('2016-02-11T10:17:34'),
+        np.datetime64('2015-11-12T11:34:22'),
+        np.datetime64('2003-03-03T00:33:15'),
+        np.datetime64('2014-07-23T15:08:05'),
+        np.datetime64('2011-01-01T07:02:01')]
 
     df = vaex.from_arrays(date=date)
     pandas_df = df.to_pandas_df()
